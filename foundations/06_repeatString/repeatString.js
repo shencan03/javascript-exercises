5a6793d2bab2f8f9904aa7ce1b7f3ba5cdf7f428
shencan03 <shencan03@gmail.com>
2026-04-02 04:44:14 +0300

    complete exercises 02 through 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(s, num) {
+  if(num < 0){
+    return "ERROR"
 
+  } else if(num === 0){
+    return ""
+
+  } else{
+    let word = s
+    for(let i = 1; i < num; i++)
+      word += s
+    
+    return word
+  }
 };
 
 // Do not edit below this line
