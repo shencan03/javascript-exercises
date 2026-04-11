0840c6fe1a35c96871f45da5cf5d22cebe5ed7c8
shencan03 <shencan03@gmail.com>
2026-04-12 00:00:01 +0300

    complete 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0
+  for (let i = 0; i < arr.length; i++)
+    sum += arr[i]
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1
+  for (let i = 0; i < arr.length; i++)
+    result *= arr[i]
+  return result
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  const numOneCopy = num1
+  for (let i = 0; i < num2 - 1; i++)	
+    num1 *= numOneCopy
+
+  return num1
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1
+  for (let i = 1; i <= num; i++)
+    result *= i
+
+  return result
 };
 
 // Do not edit below this line
