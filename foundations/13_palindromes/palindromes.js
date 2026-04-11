309a2a85c9d387d1a0df9cf40b9a6c7f7b7c9abc
shencan03 <shencan03@gmail.com>
2026-04-12 02:19:22 +0300

    complete 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const alnum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  word = word.toLowerCase().split("").filter((s) => alnum.includes(s)).join("")
 
+  let indexHead = 0
+  for(let i = word.length - 1; 0 <= i && i != indexHead ; i--){
+    if(word[indexHead] != word[i])
+      return false
+    indexHead++
+  }
+
+  return true
 };
 
-// Do not edit below this line
 module.exports = palindromes;
